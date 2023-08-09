deeb2eb109000fe4cde99c7a486825d0513c8a6f
Bandeptry <trinhvanban532004@gmail.com>
2023-08-09 23:14:22 +0700

    up date

---
+++ baiTap/src/chuyenDoiTien.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class chuyenDoiTien {
+    public static void main(String[] args) {
+        System.out.println("Nhap so tien USD can doi:");
+        Scanner scanner = new Scanner(System.in);
+        double usd = scanner.nextDouble();
+
+        int vnd = 23000;
+        double chuyenDoi = usd * 23000;
+        System.out.println("So tien da chuyen doi: "+chuyenDoi);
+    }
+}
